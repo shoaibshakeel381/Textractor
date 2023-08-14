32770c1e24bafa1bd7854737a2bc0e4a89877520
Shoaib Shakeel <shoaib.shakeel@mathem.se>
2023-08-14 11:19:19 +0200

    Fix deploy.ps1 to build stuff correctly - Fixed deploy script - Removed temporary extension 1119

--- deploy.ps1
+++ deploy.ps1
@@ -1,8 +1,34 @@
 ﻿param([string]$version="1.0.0")
 
-cd $PSScriptRoot;
+Set-Location $PSScriptRoot;
 mkdir -Force builds;
-cd builds;
+Set-Location builds;
+
+# Gather all runtime libraries
+Remove-Item -Force -Recurse -Verbose "Runtime" -ErrorAction Ignore;
+mkdir -Force -Verbose "Runtime";
+foreach ($arch in @("x86", "x64"))
+{
+	mkdir -Force -Verbose "Runtime/$arch";
+	foreach ($file in @(
+		"LoaderDll.dll",
+		"LocaleEmulator.dll",
+		"Qt5Core.dll",
+		"Qt5Gui.dll",
+		"Qt5Network.dll",
+		"Qt5WebSockets.dll",
+		"Qt5WinExtras.dll"
+		"Qt5Widgets.dll",
+		"platforms",
+		"styles"
+	))
+	{
+		Copy-Item -Force -Recurse -Destination "Runtime/$arch/$file" -Path "Release_$arch/$file";
+	}
+	Copy-Item -Force -Recurse -Destination "Runtime/$arch" -Path "C:/Program Files/Microsoft Visual Studio/2022/Preview/VC/Redist/MSVC/**/$arch/Microsoft.VC143.CRT/*"
+}
+
+# build project
 mkdir -Force x86;
 mkdir -Force x64;
 
@@ -21,16 +47,16 @@ foreach ($language in @{
 }.GetEnumerator())
 {
 	$folder = "Textractor-$($language.Value)-$version";
-	rm -Force -Recurse $folder;
+	Remove-Item -Force -Recurse $folder  -ErrorAction Ignore;
 	mkdir -Force $folder;
 
 	foreach ($arch in @("x86", "x64"))
 	{
-		cd $arch;
+		Set-Location $arch;
 		$VS_arch = if ($arch -eq "x86") {"Win32"} else {"x64"};
 		&"C:\Program Files\Microsoft Visual Studio\2022\Preview\Common7\IDE\CommonExtensions\Microsoft\CMake\CMake\bin\cmake.exe" -G "Visual Studio 17 2022" -A"$VS_arch" -DVERSION="$version" -DTEXT_LANGUAGE="$($language.Key)" -DCMAKE_BUILD_TYPE="Release" ../..;
 		&"C:\Program Files\Microsoft Visual Studio\2022\Preview\Common7\IDE\devenv" Textractor.sln /build "Release|$VS_arch";
-		cd ..;
+		Set-Location ..;
 		mkdir -Force "$folder/$arch";
 		foreach ($file in @(
 			"Textractor.exe",
@@ -38,7 +64,7 @@ foreach ($language in @{
 			"texthook.dll"
 		))
 		{
-			copy -Force -Recurse -Destination "$folder/$arch" -Path "Release_$arch/$file";
+			Copy-Item -Force -Recurse -Destination "$folder/$arch" -Path "Release_$arch/$file";
 		}
 		foreach ($extension in @(
 			"Bing Translate",
@@ -58,49 +84,28 @@ foreach ($language in @{
 			"Remove Repeated Phrases",
 			"Remove Repeated Phrases 2",
 			"Remove 30 Repeated Sentences",
-			"Remove 30 Repeated Sentences",
-			"Repeated Sentence 1119",
 			"Replacer",
 			"Styler",
 			"Sugoi Offline Translate",
 			"Thread Linker"
 		))
 		{
-			copy -Force -Recurse -Destination "$folder/$arch/$extension.xdll" -Path "Release_$arch/$extension.dll";
+			Copy-Item -Force -Recurse -Destination "$folder/$arch/$extension.xdll" -Path "Release_$arch/$extension.dll";
 		}
+
+		#&"C:\Program Files (x86)\Microsoft SDKs\ClickOnce\SignTool\signtool.exe" sign /a /v /t "http://timestamp.digicert.com"  /fd SHA256 @(Get-ChildItem "$folder\$arch\*");
 	}
-	&"C:\Program Files (x86)\Microsoft SDKs\ClickOnce\SignTool\signtool.exe" sign /a /v /t "http://timestamp.digicert.com"  /fd SHA256 @(dir "$folder\**\*");
+
+	Copy-Item -Force -Recurse -Destination $folder -Path "Runtime/*";
 }
 
-rm -Force -Recurse -Verbose "Runtime";
-mkdir -Force -Verbose "Runtime";
-foreach ($arch in @("x86", "x64"))
-{
-	mkdir -Force -Verbose "Runtime/$arch";
-	foreach ($file in @(
-		"LoaderDll.dll",
-		"LocaleEmulator.dll",
-		"Qt5Core.dll",
-		"Qt5Gui.dll",
-		"Qt5Network.dll",
-		"Qt5WebSockets.dll",
-		"Qt5WinExtras.dll"
-		"Qt5Widgets.dll",
-		"platforms",
-		"styles"
-	))
-	{
-		copy -Force -Recurse -Destination "Runtime/$arch/$file" -Path "Release_$arch/$file";
-	}
-	copy -Force -Recurse -Destination "Runtime/$arch" -Path "C:/Program Files/Microsoft Visual Studio/2022/Preview/VC/Redist/MSVC/**/$arch\Microsoft.VC143.CRT/*"
-}
 
-rm -Force -Recurse "Textractor";
-mkdir -Force "Textractor";
-copy -Force -Recurse -Destination "Textractor" -Path @("Runtime/*", "Textractor-English-$version/*");
+#Remove-Item -Force -Recurse "Textractor" -ErrorAction Ignore;
+#mkdir -Force "Textractor";
+#Copy-Item -Force -Recurse -Destination "Textractor" -Path @("Runtime/*", "Textractor-English-$version/*");
 #&"C:\Program Files\7-Zip\7z" a "Textractor-$version-Zip-Version-English-Only.zip" Textractor/ ../INSTALL_THIS_UNICODE_FONT.ttf
 
-cd ..;
+Set-Location ..;
 
 # Needs inno Setup
 #&"C:\Program Files (x86)\Inno Setup 6\iscc" -DVERSION="$version" installer.iss;
